5b42d4358ca2c91a256d541b5a96552c6f79d5f3
Daivakrupa Thota <80198240+daivakrupa@users.noreply.github.com>
2026-03-16 12:06:36 +0530

    Enable DynamoDB table for state locking

--- terraform-dev-infra/backend.tf
+++ terraform-dev-infra/backend.tf
@@ -4,8 +4,7 @@ terraform {
     key                  = "terraform.tfstate"
     region               = "us-east-1"
     encrypt              = true
-    #dynamodb_table       = "terraform-state-lock"
-    use_lockfile         = true
+    dynamodb_table       = "terraform-state-lock"
     workspace_key_prefix = "workspaces"
   }
 }
